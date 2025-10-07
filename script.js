87318af153165e80096f4e621605d131e04deef8
NIKITA NINAWE <nikitaninawe708@gmail.com>
2025-10-07 16:04:40 +0530

    feat:adds basic file structure and starter code

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add a simple scroll effect to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
